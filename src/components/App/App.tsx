975b0990fb637f2f3117176067b771301f95393d
Keith Eng <keith.eng@outlook.com>
2024-03-09 03:17:37 +0700

    UNWA: Handle and render new SearchHistoryList component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,25 +2,33 @@ import React, { useState, useEffect } from "react";
 
 import Search from '../Search/Search';
 import WeatherDisplay from '../WeatherDisplay/WeatherDisplay';
+import SearchHistoryList from '../SearchHistoryList/SearchHistoryList';
 
 import { getWeather } from "../../services/OpenWeatherMap";
-import { Weather } from '../../constants/types';
+import { Weather, History } from '../../constants/types';
 import { formatWeather } from "../../util";
 
 import './App.scss';
 
 const App: React.FC = () => {
 	const [location, setLocation] = useState("")
+	const [searchHistory, setSearchHistory] = useState<History[]>([]);
 	const [weatherData, setWeatherData] = useState<Weather>() // Opted to use useState and simple prop drilling as application is not deeply nested
 
-	useEffect(() => {
-		getWeather("Singapore")
+	const handleSearch = (searchLocation: string) => {
+		if (!searchLocation) return;
+
+		getWeather(searchLocation)
 			.then(data => formatWeather(data))
-			.then(formattedData => setWeatherData(formattedData))
+			.then(formattedData => setWeatherData(formattedData)) // For Searching we do not push into history
 			.catch(error => {
 				console.error(error)
 				return error
 			})
+	}
+
+	useEffect(() => {
+		handleSearch("singapore") // Placeholder location on mount
 	}, []);
 
 	const handleUpdate = (location: string) => {
@@ -32,17 +40,34 @@ const App: React.FC = () => {
 
 		getWeather(location)
 			.then(data => formatWeather(data))
-			.then(formattedData => setWeatherData(formattedData))
+			.then(formattedData => {
+				const history: History = {
+					city: formattedData.city,
+					time: formattedData.time
+				}
+
+				setSearchHistory((prevState) => [history, ...prevState])
+				setWeatherData(formattedData)
+			})
 			.catch(error => {
 				console.error(error)
 				return error
 			})
 	}
 
+	const handleDelete = (index: number) => {
+		setSearchHistory((prevState) => prevState.filter((_, i) => i !== index))
+	}
+
 	return (
 		<div>
 			<Search location={location} handleUpdate={handleUpdate} handleClick={handleClick} />
 			{weatherData && <WeatherDisplay weatherData={weatherData} />}
+			<SearchHistoryList
+				searchHistory={searchHistory}
+				handleDelete={handleDelete}
+				handleSearch={handleSearch}
+			/>
 		</div>
 	);
 }
