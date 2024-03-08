5b38bd021f466cc6b5a2cc6d15749b2c2eaf39b4
Keith Eng <keith.eng@outlook.com>
2024-03-09 02:04:44 +0700

    UNWA: Refactor code and render WeatherDisplay

--- src/services/OpenWeatherMap/index.ts
+++ src/services/OpenWeatherMap/index.ts
@@ -4,9 +4,12 @@ export function getWeather(location: string) {
     const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY || '';
 
     return fetch(`${OPENWEATHERMAP_API_URL}/data/2.5/weather?q=${location}&appid=${API_KEY}`)
-        .then(response => response.json())
-        .catch((Error) => {
-            console.error(Error);
-            return Error;
+        .then(response => {
+            if (!response.ok) throw new Error("City/Country not found!");
+            return response.json()
+        })
+        .catch((e) => {
+            console.error(e);
+            throw new Error(e);
         })
 }
